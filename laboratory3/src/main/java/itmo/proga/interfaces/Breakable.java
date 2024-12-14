53d452d362493928123049284db7d0909b88b59d
ArteMyasnik <artemyasnik.git@mail.ru>
2024-12-14 21:43:16 +0300

    Start making interfaces

--- laboratory3/src/main/java/itmo/proga/interfaces/Breakable.java
+++ laboratory3/src/main/java/itmo/proga/interfaces/Breakable.java
@@ -1,4 +1,7 @@
 package itmo.proga.interfaces;
 
 public interface Breakable {
+    public boolean isBroken = false;
+    public void breakItem();
+    public boolean isBroken();
 }
--- laboratory3/src/main/java/itmo/proga/interfaces/Drivable.java
+++ laboratory3/src/main/java/itmo/proga/interfaces/Drivable.java
@@ -1,4 +1,8 @@
 package itmo.proga.interfaces;
 
 public interface Drivable {
+    public void drive();
+    public void snort();
+    public void stop();
+    public void accelerate();
 }
